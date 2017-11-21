6299021f5fef231b86140bcde1c477149bbda28d
spolloni <stefano_polloni@brown.edu>
2017-11-20 21:14:43 -0500

    Running main.py on AWS Server at 21:14--Nov11

--- LIGHTSTONE/runmain.sh
+++ LIGHTSTONE/runmain.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
-git status
+#push code to git
 git commit -a -m "Running main.py on AWS Server at $(date +%H:%M--%h%m)"
 git push
 
+#ssh into server
 ssh -i "/Users/stefanopolloni/SAkey.pem" \
 ubuntu@ec2-18-216-131-130.us-east-2.compute.amazonaws.com \
 'bash -s' << \EOF
@@ -11,10 +12,11 @@ ubuntu@ec2-18-216-131-130.us-east-2.compute.amazonaws.com \
 #CD into code directory
 cd $HOME/analysis/Code
 
-#update from git 
+#pull code from git 
 git pull
 
 # run main.py
+python /LIGHSTONE/main.py
 
 
 EOF
