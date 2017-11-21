4cfb1c5ad88ed3435ea6761ae024d8bfc1ced45f
spolloni <stefano_polloni@brown.edu>
2017-11-20 21:32:37 -0500

    Running main.py on AWS Server at 21:32--Nov11

--- LIGHTSTONE/main.py
+++ LIGHTSTONE/main.py
@@ -190,10 +190,7 @@ if _5_a_PLOTS_ == 1:
     dofile = "subcode/export2gradplot.do"
     cmd = ['stata-mp','do',dofile,rdp,salgo,
                 spar1,spar2,sbw,typ,gendata]
-    #subprocess.call(cmd)
-
-    cwd = os.getcwd()
-    print cwd 
+    subprocess.call(cmd)
 
 if _5_b_PLOTS_ == 1:
 
