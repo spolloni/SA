ca11ba4b4d34f91c0dc3b41400d8624931c0e1b9
spolloni <stefano_polloni@brown.edu>
2017-11-21 14:45:56 -0500

    Running main.py on AWS Server at 14:45--Nov11

--- LIGHTSTONE/runmain.sh
+++ LIGHTSTONE/runmain.sh
@@ -5,22 +5,22 @@ git commit -a -m "Running main.py on AWS Server at $(date +%H:%M--%h%m)"
 git push
 
 #ssh into server
-ssh -i "/Users/stefanopolloni/SAkey.pem" \
-ubuntu@ec2-18-216-234-87.us-east-2.compute.amazonaws.com \
-'source ~/.profile; bash -s' << \EOF
-
-	#CD into code directory
-	cd $HOME/analysis/Code
-
-	#pull code from git 
-	git pull
-
-	# run main.py
-	cd $HOME/analysis/Code/LIGHTSTONE
-	python main.py
-
-	#echo "$LD_LIBRARY_PATH"
-
-EOF
+#ssh -i "/Users/stefanopolloni/SAkey.pem" \
+#ubuntu@ec2-18-216-234-87.us-east-2.compute.amazonaws.com \
+#'source ~/.profile; bash -s' << \EOF
+#
+#	#CD into code directory
+#	cd $HOME/analysis/Code
+#
+#	#pull code from git 
+#	git pull
+#
+#	# run main.py
+#	cd $HOME/analysis/Code/LIGHTSTONE
+#	python main.py
+#
+#	#echo "$LD_LIBRARY_PATH"
+#
+#EOF
 
 echo $CWD
