f08bb07d5504b9f9e6513e33a1add2763ce7378f
spolloni <stefano_polloni@brown.edu>
2017-11-21 13:46:54 -0500

    Running main.py on AWS Server at 13:46--Nov11

--- LIGHTSTONE/runmain.sh
+++ LIGHTSTONE/runmain.sh
@@ -9,17 +9,16 @@ ssh -i "/Users/stefanopolloni/SAkey.pem" \
 ubuntu@ec2-18-216-234-87.us-east-2.compute.amazonaws.com \
 'source ~/.profile; bash -s' << \EOF
 
-#CD into code directory
-cd $HOME/analysis/Code
+	#CD into code directory
+	cd $HOME/analysis/Code
 
-#pull code from git 
-git pull
+	#pull code from git 
+	git pull
 
-# run main.py
-cd $HOME/analysis/Code/LIGHTSTONE
-python main.py
+	# run main.py
+	cd $HOME/analysis/Code/LIGHTSTONE
+	python main.py
 
-#echo "$LD_LIBRARY_PATH"
-
+	#echo "$LD_LIBRARY_PATH"
 
 EOF
