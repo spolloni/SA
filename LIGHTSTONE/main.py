eb6961ab886ce884c6498632a8bc7b9104cac66c
spolloni <stefano_polloni@brown.edu>
2017-12-13 18:02:39 -0500

    Running main.py on AWS Server at 18:02--Dec12

--- LIGHTSTONE/main.py
+++ LIGHTSTONE/main.py
@@ -46,12 +46,12 @@ workers = int(multiprocessing.cpu_count()-1)
 # SWITCHBOARD  # 
 ################
 
-_1_a_IMPORT = 1 
+_1_a_IMPORT = 0 
 _1_b_IMPORT = 0 
 
-_2_FLAGRDP_ = 0
+_2_FLAGRDP_ = 1
 
-_3_CLUSTER_ = 0 
+_3_CLUSTER_ = 1 
 algo = 1         # Algo for Cluster 1=DBSCAN, 2=HDBSCAM
 par1 = 0.002     # Parameter setting #1 for Clustering                          
 par2 = 10        # Parameter setting #2 for Clustering 
