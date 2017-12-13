16d71ae55fdf40604d12bfd79b145a406a85e3f0
spolloni <stefano_polloni@brown.edu>
2017-12-13 13:02:53 -0500

    Running main.py on AWS Server at 13:02--Dec12

--- LIGHTSTONE/runmain.sh
+++ LIGHTSTONE/runmain.sh
@@ -10,22 +10,22 @@ RET_DBASE=0
 git commit -a -m "Running main.py on AWS Server at $(date +%H:%M--%h%m)"
 git push
 
-#ssh into server
-#ssh -i $HOME/SAkey.pem \
-#ubuntu@ec2-18-216-234-87.us-east-2.compute.amazonaws.com \
-#'source ~/.profile; bash -s' << \EOF
-#
-#	#CD into code directory
-#	cd $HOME/analysis/Code
-#
-#	#pull code from git 
-#	git pull
-#
-#	# run main.py
-#	cd $HOME/analysis/Code/LIGHTSTONE
-#	python main.py
-#
-#EOF
+ssh into server
+ssh -i $HOME/SAkey.pem \
+ubuntu@ec2-18-216-234-87.us-east-2.compute.amazonaws.com \
+'source ~/.profile; bash -s' << \EOF
+
+	#CD into code directory
+	cd $HOME/analysis/Code
+
+	#pull code from git 
+	git pull
+
+	# run main.py
+	cd $HOME/analysis/Code/LIGHTSTONE
+	python main.py
+
+EOF
 
 if [ $RET_GRADPLOTS = 1 ]; then
 
