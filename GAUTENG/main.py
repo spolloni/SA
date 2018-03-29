64181343196627df4e0296afe2833d65de3469d5
William Violette <william_violette@brown.edu>
2018-03-29 11:35:34 -0400

    add GHS works

--- GAUTENG/main.py
+++ GAUTENG/main.py
@@ -55,17 +55,18 @@ _1_a_IMPORT = 0  # import LIGHTSTONE
 _1_b_IMPORT = 0  # import BBLU
 _1_c_IMPORT = 0  # import CENSUS
 _1_d_IMPORT = 0  # import GCRO + landplots
+_1_e_IMPORT = 1  # import GHS
 
 _2_FLAGRDP_ = 1
 
-_3_CLUSTER_ = 1 
+_3_CLUSTER_ = 0 
 rdp  = 'all'     # Choose rdp definition. 
 algo = 1         # Algo for Cluster 1=DBSCAN, 2=HDBSCAM #1
 par1 = 700       # Parameter setting #1 for Clustering  #750,700                       
 par2 = 50        # Parameter setting #2 for Clustering  #77,50
 
-_4_a_DISTS_ = 1
-_4_b_DISTS_ = 1
+_4_a_DISTS_ = 0
+_4_b_DISTS_ = 0
 bw  = 1200       # bandwidth for clusters
 sig = 3          # sigma factor for concave hulls
 
@@ -174,6 +175,15 @@ if _1_d_IMPORT ==1:
 
     print '\n'," - GCRO data: done! "'\n'
 
+if _1_e_IMPORT ==1:
+
+    print '\n'," Importing GHS into SQL... ",'\n'
+    
+    dofile = "subcode/add_ghs.do"
+    cmd = ['stata-mp', 'do', dofile]
+    subprocess.call(cmd)    
+
+    print '\n'," - GHS data: done! "'\n'
 
 #############################################
 # STEP 2:  flag RDP properties              #
